2a12c55afb3a6edc6938c4ce556e59c2a5cb120f
Kenji Miyake <31987104+kenji-miyake@users.noreply.github.com>
2019-07-28 22:15:22 +0900

    Setup VSCode environment

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # procon-atcoder-python
+
+## Overview
+
+Python version of [kenji-miyake/procon-atcoder-cpp](https://github.com/kenji-miyake/procon-atcoder-cpp).
---
+++ template.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+import sys
+from typing import List, Optional
+
+import numpy as np
+
+
+def solve():
+    # TODO: implement
+    pass
+
+
+def main():
+    # TODO: input
+    solve()
+
+
+if __name__ == "__main__":
+    main()
